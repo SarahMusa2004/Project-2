5616afff0e173d68d1df70babe2ba1d0a1b65e9f
sarah <ps242956@edu.summacollege.nl>
2023-06-16 14:39:39 +0200

    Homepage gemaakt

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>D66</title>
+</head>
+<body >
+  <header id="header-homepage">
+      <h1>Homepagina</h1>
+      <img id="logo" src="images\D66-stop-stilstand-stem-vooruit-og2.png"></img>
+      <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="">Leden</a>
+      <a href="">Standpunten</a>
+      <a href="">Nieuws</a>
+      <a href="">Inschrijven</a>
+      </div>
+  
+  </header>
+  <article id="article-home">
+    <div id="energietransitie">
+      <h3>Klimaat aanpakken. Dan gaan we vooruit</h3>
+      <p>D66 wil een schoon en veilig land met gezonde natuur. <br/>
+         Daarom grijpen we nu stevig in voor het klimaat. <br/>
+         Want de partijen die klimaat als gedoe zien, <br/>
+         zorgen voor stilstand. Aanpakken en eraan verdienen, <br/>
+         dan gaan we vooruit. 
+      </p>
+    </div>
+    <img id="energietransitie-afb" src="images\d66-themas-windmolens-op-zee-1024x576.webp" alt="">
+    <div id="natuur">
+      <h3>Een revolutie in de landbouw brengt ons vooruit</h3>
+      <p>
+      Nederland wordt geteisterd door de stikstofcrisis. <br/>
+      Een crisis die ons land in de greep houdt. <br/>
+      De natuur staat onder druk en de bouw van broodnodige woningen loopt ernstige vertraging op. <br/>
+      Boeren krijgen niet een eerlijke prijs voor hun voedsel. <br/>
+      Dit alles leidt tot stilstand. Inzetten op dierenwelzijn, <br/>
+      biodiversiteit en een revolutie in de landbouw brengt ons vooruit.
+      </p>
+    </div>
+    <img id="natuur-afb" src="images\d66-mensen-tjeerd-de-groot-1024x576.webp" alt="">
+    <div id="onderwijs">
+      <h3>Onderwijs op maat helpt kinderen vooruit</h3>
+      <p>
+      D66 wil goed onderwijs voor alle kinderen. <br/>
+      Dat helpt Nederland vooruit. <br/>
+      Met een sterke economie en kansen voor iedereen. <br/>
+      Daarom willen wij het geld dat we nu samen verdienen uitgeven aan passend onderwijs. <br/>
+      We zorgen voor meer aandacht voor kinderen door kleinere klassen. <br/>
+      Te vroeg keuzes laten maken? Stilstand. <br/>
+      Onderwijs op maat helpt kinderen vooruit.
+      </p>
+    </div>
+    <img id="onderwijs-afb" src="images\200302-D66_SchoolreisBijlmer-044-JeroenMooijman-1024x683.webp" alt="">
+    <div id="wonen">
+      <h3>Met een thuis voor iedereen gaan we vooruit</h3>
+      <p>D66 wil dat alle mensen een betaalbaar huis kunnen vinden in de buurt waar ze thuis zijn. 
+        Wij willen vóór 2030 meer dan een miljoen huizen bouwen. 
+        Dat is de enige manier om aan de groeiende vraag te voldoen. 
+        Vooral voor mensen met een middeninkomen is er een groot tekort aan betaalbare woningen.
+      </p>
+    </div>
+    <img id="wonen-afb" src="images\d66-wonen_gezin_huis_deur_iStock-1024x576.webp" alt="">
+    <div id="leden-link">
+      <h3>Bekijk onze leden en word zelf een lid!</h3>
+      <img id="alle-leden" src="images\d66-mensen-compleet-1024x576.jpeg" alt="">
+    </div>
+    <div id="standpunten-link">
+      <h3>Lees hier onze standpunten</h3>
+      <img id="standpunten-link-afb" src="images\voor-web-D66-verhouding-795x636-fotos-advertenties-december-2021-scaled.jpg" alt="">
+    </div>
+    <div id="nieuwsbericht1">
+      <h3>Reactie Partijvoorzitter en Partijleider op vertrek Sophie in ‘t Veld</h3>
+      <p>Europarlementariër Sophie in ‘t Veld heeft besloten D66 te verlaten. <br/>
+         Vanmorgen heeft ze het partijbestuur daarover geïnformeerd.
+      </p>
+      <a href="https://d66.nl/nieuws/reactie-partijvoorzitter-en-partijleider-op-vertrek-sophie-in-t-veld/">Lees verder</a>
+    </div>
+    <div id="nieuwsbericht2">
+      <h3>Politiek en publieke organisaties vormen gezamenlijk front tegen agressie en geweld</h3>
+      <p>
+      Maatschappij en politiek slaan de handen ineen tegen agressie en geweld. <br/>
+      In een maatschappelijk breed gedragen manifest laten 25 ondertekenaars zien dat de samenleving als één blok achter deze groep zou moeten staan. <br/>
+      De deelnemers voelen samen verantwoordelijkheid om ons land veilig te houden en de trend te keren.
+      </p>
+      <a href="https://d66.nl/nieuws/gezamenlijk-front-tegen-agressie-en-geweld/">Lees verder</a>
+    </div>
+  </article>
+  <footer id="footer">
+    <div id="contact">
+      <h2>Contact</h2>
+      <h3>Landelijke Bureau D66</h3>
+      <p>
+         Lange Houtstraat 11 <br/>
+         2511 CV Den Haag <br/>
+      </p>
+      <h3>Telefoonnummer</h3>
+      <p>070-3566066</p>
+    </div>
+  </footer>
+</body>
+</html>
